3c5a9f4ea8e3c72af69d4c97e852ec046dd835de
Niklas Natter <niklas@sulu.io>
2019-01-08 11:56:39 +0100

    implemented seo-controller, find-seo-query and basic modify-seo-message

---
+++ Controller/SeoController.php
@@ -0,0 +1,91 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of Sulu.
+ *
+ * (c) Sulu GmbH
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.
+ */
+
+namespace Sulu\Bundle\ContentBundle\Controller;
+
+use FOS\RestBundle\Controller\ControllerTrait;
+use FOS\RestBundle\Routing\ClassResourceInterface;
+use FOS\RestBundle\View\ViewHandlerInterface;
+use Sulu\Bundle\ContentBundle\Model\Content\Exception\ContentNotFoundException;
+use Sulu\Bundle\ContentBundle\Model\Content\Message\ModifyContentMessage;
+use Sulu\Bundle\ContentBundle\Model\Content\Message\ModifySeoMessage;
+use Sulu\Bundle\ContentBundle\Model\Content\Query\FindContentQuery;
+use Sulu\Bundle\ContentBundle\Model\Seo\Query\FindSeoQuery;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+use Symfony\Component\Messenger\MessageBusInterface;
+
+abstract class SeoController implements ClassResourceInterface
+{
+    use ControllerTrait;
+
+    /**
+     * @var MessageBusInterface
+     */
+    protected $messageBus;
+
+    /**
+     * @var string
+     */
+    protected $resourceKey;
+
+    public function __construct(
+        MessageBusInterface $messageBus,
+        ViewHandlerInterface $viewHandler
+    ) {
+        $this->messageBus = $messageBus;
+
+        $this->setViewHandler($viewHandler);
+    }
+
+    public function cgetAction(): Response
+    {
+        throw new NotFoundHttpException();
+    }
+
+    public function getAction(Request $request, string $resourceId): Response
+    {
+        $message = new FindSeoQuery($this->getResourceKey(), $resourceId, $request->query->get('locale'));
+        $this->messageBus->dispatch($message);
+        $seo = $message->getSeo();
+
+        return $this->handleView($this->view($seo));
+    }
+
+    public function putAction(Request $request, string $resourceId): Response
+    {
+        $locale = $request->query->get('locale');
+        $message = new ModifySeoMessage($this->getResourceKey(), $resourceId, $locale, $request->request->all());
+        $this->messageBus->dispatch($message);
+        $seo = $message->getSeo();
+
+        $action = $request->query->get('action');
+        if ($action) {
+            $this->handleAction($resourceId, $locale, $action);
+        }
+
+        return $this->handleView($this->view($seo));
+    }
+
+    protected function handleAction(string $resourceId, string $locale, string $action): void
+    {
+        if ('publish' === $action) {
+            $this->handlePublish($resourceId, $locale);
+        }
+    }
+
+    abstract protected function handlePublish(string $resourceId, string $locale): void;
+
+    abstract protected function getResourceKey(): string;
+}
---
+++ Model/Seo/Message/ModifySeoMessage.php
@@ -0,0 +1,85 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of Sulu.
+ *
+ * (c) Sulu GmbH
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.
+ */
+
+namespace Sulu\Bundle\ContentBundle\Model\Content\Message;
+
+use Sulu\Bundle\ContentBundle\Common\Model\MissingResultException;
+use Sulu\Bundle\ContentBundle\Common\Payload\PayloadTrait;
+use Sulu\Bundle\ContentBundle\Model\Seo\SeoInterface;
+
+class ModifySeoMessage
+{
+    use PayloadTrait {
+        __construct as protected initializePayload;
+    }
+
+    /**
+     * @var string
+     */
+    private $resourceKey;
+
+    /**
+     * @var string
+     */
+    private $resourceId;
+
+    /**
+     * @var string
+     */
+    private $locale;
+
+    /**
+     * @var SeoInterface|null
+     */
+    private $seo;
+
+    public function __construct(string $resourceKey, string $resourceId, string $locale, array $payload)
+    {
+        $this->resourceKey = $resourceKey;
+        $this->resourceId = $resourceId;
+        $this->locale = $locale;
+
+        $this->initializePayload($payload);
+    }
+
+    public function getResourceKey(): string
+    {
+        return $this->resourceKey;
+    }
+
+    public function getResourceId(): string
+    {
+        return $this->resourceId;
+    }
+
+    public function getLocale(): string
+    {
+        return $this->locale;
+    }
+
+    public function getSeo(): SeoInterface
+    {
+        if (!$this->seo) {
+            throw new MissingResultException(__METHOD__);
+        }
+
+        return $this->seo;
+    }
+
+    public function setSeo(SeoInterface $seo): ModifySeoMessage
+    {
+        $this->seo = $seo;
+
+        return $this;
+    }
+}
