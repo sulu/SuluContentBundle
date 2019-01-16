cc5897bd5753eb036351499b3011736eee14172d
Niklas Natter <niklas@sulu.io>
2019-01-16 16:37:39 +0100

    implemented excerpt-view-factory and excerpt-view-factory-interface

---
+++ Model/Excerpt/Factory/ExcerptViewFactoryInterface.php
@@ -0,0 +1,27 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of Sulu.
+ *
+ * (c) Sulu GmbH
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.
+ */
+
+namespace Sulu\Bundle\ContentBundle\Model\Excerpt\Factory;
+
+use Sulu\Bundle\ContentBundle\Model\Excerpt\ExcerptDimensionInterface;
+use Sulu\Bundle\ContentBundle\Model\Excerpt\ExcerptViewInterface;
+
+interface ExcerptViewFactoryInterface
+{
+    /**
+     * @param ExcerptDimensionInterface[] $excerptDimensions
+     *
+     * @return ExcerptViewInterface|null
+     */
+    public function create(array $excerptDimensions, string $locale): ?ExcerptViewInterface;
+}
