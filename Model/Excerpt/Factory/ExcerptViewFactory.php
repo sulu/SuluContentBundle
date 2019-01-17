5b77ded43f3c7159857a970590e6741e9c463bd7
Niklas Natter <niklas@sulu.io>
2019-01-17 11:37:14 +0100

    clear images and icons in modify-excerpt-message-handler, fixed class-name in excerpt serializer config, adjusted logic in excerpt-view-factory

--- Model/Excerpt/Factory/ExcerptViewFactory.php
+++ Model/Excerpt/Factory/ExcerptViewFactory.php
@@ -42,10 +42,30 @@ class ExcerptViewFactory implements ExcerptViewFactoryInterface
             $title = $excerptDimension->getTitle() ?? $title;
             $more = $excerptDimension->getMore() ?? $more;
             $description = $excerptDimension->getDescription() ?? $description;
-            $categories = array_merge($categories, $excerptDimension->getCategories());
-            $tags = array_merge($tags, $excerptDimension->getTags());
-            $icons = array_merge($icons, $excerptDimension->getIcons());
-            $images = array_merge($images, $excerptDimension->getImages());
+
+            foreach ($excerptDimension->getCategories() as $dimensionCategory) {
+                if (!in_array($dimensionCategory, $categories, true)) {
+                    $categories[] = $dimensionCategory;
+                }
+            }
+
+            foreach ($excerptDimension->getTags() as $dimensionTag) {
+                if (!in_array($dimensionTag, $tags, true)) {
+                    $tags[] = $dimensionTag;
+                }
+            }
+
+            foreach ($excerptDimension->getIcons() as $dimensionIcon) {
+                if (!in_array($dimensionIcon, $icons, true)) {
+                    $icons[] = $dimensionIcon;
+                }
+            }
+
+            foreach ($excerptDimension->getImages() as $dimensionImage) {
+                if (!in_array($dimensionImage, $images, true)) {
+                    $images[] = $dimensionImage;
+                }
+            }
         }
 
         return new ExcerptView(
@@ -55,10 +75,10 @@ class ExcerptViewFactory implements ExcerptViewFactoryInterface
             $title,
             $more,
             $description,
-            array_unique($categories),
-            array_unique($tags),
-            array_unique($icons),
-            array_unique($images)
+            $categories,
+            $tags,
+            $icons,
+            $images
         );
     }
 }
