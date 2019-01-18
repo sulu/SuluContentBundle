6b8ae51a2064b6866a9ad7132cee2ef64eea6bf9
Niklas Natter <niklas@sulu.io>
2019-01-18 11:16:27 +0100

    added test-cases for messages and queries in excerpt namespace

---
+++ Tests/Unit/Model/Excerpt/Message/ModifyExcerptMessageTest.php
@@ -0,0 +1,269 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of Sulu.
+ *
+ * (c) Sulu GmbH
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.
+ */
+
+namespace Sulu\Bundle\ContentBundle\Tests\Unit\Model\Excerpt\Message;
+
+use PHPUnit\Framework\TestCase;
+use Sulu\Bundle\ContentBundle\Common\Model\MissingResultException;
+use Sulu\Bundle\ContentBundle\Model\Excerpt\ExcerptViewInterface;
+use Sulu\Bundle\ContentBundle\Model\Excerpt\Message\ModifyExcerptMessage;
+
+class ModifyExcerptMessageTest extends TestCase
+{
+    const RESOURCE_KEY = 'test_resource_excerpts';
+
+    public function testGetResourceKey(): void
+    {
+        $message = new ModifyExcerptMessage(
+            self::RESOURCE_KEY,
+            'resource-1',
+            'en',
+            [
+                'title' => 'title-1',
+                'more' => 'more-1',
+                'description' => null,
+                'categories' => [1, 2, 3],
+                'tags' => ['tag-1', 'tag-2'],
+                'icons' => ['ids' => []],
+                'images' => ['ids' => [5, 6]],
+            ]
+        );
+
+        $this->assertEquals(self::RESOURCE_KEY, $message->getResourceKey());
+    }
+
+    public function testGetResourceId(): void
+    {
+        $message = new ModifyExcerptMessage(
+            self::RESOURCE_KEY,
+            'resource-1',
+            'en',
+            [
+                'title' => 'title-1',
+                'more' => 'more-1',
+                'description' => null,
+                'categories' => [1, 2, 3],
+                'tags' => ['tag-1', 'tag-2'],
+                'icons' => ['ids' => []],
+                'images' => ['ids' => [5, 6]],
+            ]
+        );
+
+        $this->assertEquals('resource-1', $message->getResourceId());
+    }
+
+    public function testGetLocale(): void
+    {
+        $message = new ModifyExcerptMessage(
+            self::RESOURCE_KEY,
+            'resource-1',
+            'en',
+            [
+                'title' => 'title-1',
+                'more' => 'more-1',
+                'description' => null,
+                'categories' => [1, 2, 3],
+                'tags' => ['tag-1', 'tag-2'],
+                'icons' => ['ids' => []],
+                'images' => ['ids' => [5, 6]],
+            ]
+        );
+
+        $this->assertEquals('en', $message->getLocale());
+    }
+
+    public function testGetTitle(): void
+    {
+        $message = new ModifyExcerptMessage(
+            self::RESOURCE_KEY,
+            'resource-1',
+            'en',
+            [
+                'title' => 'title-1',
+                'more' => 'more-1',
+                'description' => null,
+                'categories' => [1, 2, 3],
+                'tags' => ['tag-1', 'tag-2'],
+                'icons' => ['ids' => []],
+                'images' => ['ids' => [5, 6]],
+            ]
+        );
+
+        $this->assertEquals('title-1', $message->getTitle());
+    }
+
+    public function testGetMore(): void
+    {
+        $message = new ModifyExcerptMessage(
+            self::RESOURCE_KEY,
+            'resource-1',
+            'en',
+            [
+                'title' => 'title-1',
+                'more' => 'more-1',
+                'description' => null,
+                'categories' => [1, 2, 3],
+                'tags' => ['tag-1', 'tag-2'],
+                'icons' => ['ids' => []],
+                'images' => ['ids' => [5, 6]],
+            ]
+        );
+
+        $this->assertEquals('more-1', $message->getMore());
+    }
+
+    public function testGetDescription(): void
+    {
+        $message = new ModifyExcerptMessage(
+            self::RESOURCE_KEY,
+            'resource-1',
+            'en',
+            [
+                'title' => 'title-1',
+                'more' => 'more-1',
+                'description' => null,
+                'categories' => [1, 2, 3],
+                'tags' => ['tag-1', 'tag-2'],
+                'icons' => ['ids' => []],
+                'images' => ['ids' => [5, 6]],
+            ]
+        );
+
+        $this->assertNull($message->getDescription());
+    }
+
+    public function testGetCategoryIds(): void
+    {
+        $message = new ModifyExcerptMessage(
+            self::RESOURCE_KEY,
+            'resource-1',
+            'en',
+            [
+                'title' => 'title-1',
+                'more' => 'more-1',
+                'description' => null,
+                'categories' => [1, 2, 3],
+                'tags' => ['tag-1', 'tag-2'],
+                'icons' => ['ids' => []],
+                'images' => ['ids' => [5, 6]],
+            ]
+        );
+
+        $this->assertEquals([1, 2, 3], $message->getCategoryIds());
+    }
+
+    public function testGetTagNames(): void
+    {
+        $message = new ModifyExcerptMessage(
+            self::RESOURCE_KEY,
+            'resource-1',
+            'en',
+            [
+                'title' => 'title-1',
+                'more' => 'more-1',
+                'description' => null,
+                'categories' => [1, 2, 3],
+                'tags' => ['tag-1', 'tag-2'],
+                'icons' => ['ids' => []],
+                'images' => ['ids' => [5, 6]],
+            ]
+        );
+
+        $this->assertEquals(['tag-1', 'tag-2'], $message->getTagNames());
+    }
+
+    public function testGetIconMediaIds(): void
+    {
+        $message = new ModifyExcerptMessage(
+            self::RESOURCE_KEY,
+            'resource-1',
+            'en',
+            [
+                'title' => 'title-1',
+                'more' => 'more-1',
+                'description' => null,
+                'categories' => [1, 2, 3],
+                'tags' => ['tag-1', 'tag-2'],
+                'icons' => ['ids' => []],
+                'images' => ['ids' => [5, 6]],
+            ]
+        );
+
+        $this->assertEquals([], $message->getIconMediaIds());
+    }
+
+    public function testGetImageMediaIds(): void
+    {
+        $message = new ModifyExcerptMessage(
+            self::RESOURCE_KEY,
+            'resource-1',
+            'en',
+            [
+                'title' => 'title-1',
+                'more' => 'more-1',
+                'description' => null,
+                'categories' => [1, 2, 3],
+                'tags' => ['tag-1', 'tag-2'],
+                'icons' => ['ids' => []],
+                'images' => ['ids' => [5, 6]],
+            ]
+        );
+
+        $this->assertEquals([5, 6], $message->getImageMediaIds());
+    }
+
+    public function testGetExcerpt(): void
+    {
+        $this->expectException(MissingResultException::class);
+
+        $message = new ModifyExcerptMessage(
+            self::RESOURCE_KEY,
+            'resource-1',
+            'en',
+            [
+                'title' => 'title-1',
+                'more' => 'more-1',
+                'description' => null,
+                'categories' => [1, 2, 3],
+                'tags' => ['tag-1', 'tag-2'],
+                'icons' => ['ids' => []],
+                'images' => ['ids' => [5, 6]],
+            ]
+        );
+
+        $message->getExcerpt();
+    }
+
+    public function testSetExcerpt(): void
+    {
+        $message = new ModifyExcerptMessage(
+            self::RESOURCE_KEY,
+            'resource-1',
+            'en',
+            [
+                'title' => 'title-1',
+                'more' => 'more-1',
+                'description' => null,
+                'categories' => [1, 2, 3],
+                'tags' => ['tag-1', 'tag-2'],
+                'icons' => ['ids' => []],
+                'images' => ['ids' => [5, 6]],
+            ]
+        );
+
+        $excerpt = $this->prophesize(ExcerptViewInterface::class);
+
+        $this->assertEquals($message, $message->setExcerpt($excerpt->reveal()));
+        $this->assertEquals($excerpt->reveal(), $message->getExcerpt());
+    }
+}
---
+++ Tests/Unit/Model/Excerpt/Message/RemoveExcerptMessageTest.php
@@ -0,0 +1,36 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of Sulu.
+ *
+ * (c) Sulu GmbH
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.
+ */
+
+namespace Sulu\Bundle\ContentBundle\Tests\Unit\Model\Excerpt\Message;
+
+use PHPUnit\Framework\TestCase;
+use Sulu\Bundle\ContentBundle\Model\Excerpt\Message\RemoveExcerptMessage;
+
+class RemoveExcerptMessageTest extends TestCase
+{
+    const RESOURCE_KEY = 'test_resource_excerpts';
+
+    public function testGetResourceKey(): void
+    {
+        $message = new RemoveExcerptMessage(self::RESOURCE_KEY, 'resource-1');
+
+        $this->assertEquals(self::RESOURCE_KEY, $message->getResourceKey());
+    }
+
+    public function testGetResourceId(): void
+    {
+        $message = new RemoveExcerptMessage(self::RESOURCE_KEY, 'resource-1');
+
+        $this->assertEquals('resource-1', $message->getResourceId());
+    }
+}
