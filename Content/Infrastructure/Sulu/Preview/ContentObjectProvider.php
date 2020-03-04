e127ab97db3161de808d2e5e23e1fee2c2406fed
Luca Rath-Heel <luca@sulu.io>
2020-03-04 16:23:36 +0100

    enhance contentObjectProvider

--- Content/Infrastructure/Sulu/Preview/ContentObjectProvider.php
+++ Content/Infrastructure/Sulu/Preview/ContentObjectProvider.php
@@ -15,10 +15,11 @@ namespace Sulu\Bundle\ContentBundle\Content\Infrastructure\Sulu\Preview;
 
 use Doctrine\ORM\EntityManagerInterface;
 use Doctrine\ORM\NoResultException;
+use Sulu\Bundle\ContentBundle\Content\Application\ContentDataMapper\DataMapper\ExcerptDataMapper;
+use Sulu\Bundle\ContentBundle\Content\Application\ContentDataMapper\DataMapper\SeoDataMapper;
+use Sulu\Bundle\ContentBundle\Content\Application\ContentDataMapper\DataMapper\TemplateDataMapper;
 use Sulu\Bundle\ContentBundle\Content\Application\ContentResolver\ContentResolverInterface;
 use Sulu\Bundle\ContentBundle\Content\Domain\Exception\ContentNotFoundException;
-use Sulu\Bundle\ContentBundle\Content\Domain\Factory\CategoryFactoryInterface;
-use Sulu\Bundle\ContentBundle\Content\Domain\Factory\TagFactoryInterface;
 use Sulu\Bundle\ContentBundle\Content\Domain\Model\ContentProjectionInterface;
 use Sulu\Bundle\ContentBundle\Content\Domain\Model\ContentRichEntityInterface;
 use Sulu\Bundle\ContentBundle\Content\Domain\Model\DimensionInterface;
@@ -28,7 +29,6 @@ use Sulu\Bundle\ContentBundle\Content\Domain\Model\TemplateInterface;
 use Sulu\Bundle\PreviewBundle\Preview\Object\PreviewObjectProviderInterface;
 use Sulu\Component\Content\Compat\Structure\LegacyPropertyFactory;
 use Sulu\Component\Content\Metadata\Factory\StructureMetadataFactoryInterface;
-use Webmozart\Assert\Assert;
 
 class ContentObjectProvider implements PreviewObjectProviderInterface
 {
@@ -53,14 +53,19 @@ class ContentObjectProvider implements PreviewObjectProviderInterface
     private $contentResolver;
 
     /**
-     * @var TagFactoryInterface
+     * @var TemplateDataMapper
      */
-    private $tagFactory;
+    private $templateDataMapper;
 
     /**
-     * @var CategoryFactoryInterface
+     * @var SeoDataMapper
      */
-    private $categoryFactory;
+    private $seoDataMapper;
+
+    /**
+     * @var ExcerptDataMapper
+     */
+    private $excerptDataMapper;
 
     /**
      * @var string
@@ -72,16 +77,18 @@ class ContentObjectProvider implements PreviewObjectProviderInterface
         StructureMetadataFactoryInterface $structureMetadataFactory,
         LegacyPropertyFactory $propertyFactory,
         ContentResolverInterface $contentResolver,
-        TagFactoryInterface $tagFactory,
-        CategoryFactoryInterface $categoryFactory,
+        TemplateDataMapper $templateDataMapper,
+        SeoDataMapper $seoDataMapper,
+        ExcerptDataMapper $excerptDataMapper,
         string $entityClass
     ) {
         $this->entityManager = $entityManager;
         $this->structureMetadataFactory = $structureMetadataFactory;
         $this->propertyFactory = $propertyFactory;
         $this->contentResolver = $contentResolver;
-        $this->tagFactory = $tagFactory;
-        $this->categoryFactory = $categoryFactory;
+        $this->templateDataMapper = $templateDataMapper;
+        $this->seoDataMapper = $seoDataMapper;
+        $this->excerptDataMapper = $excerptDataMapper;
         $this->entityClass = $entityClass;
     }
 
@@ -126,197 +133,21 @@ class ContentObjectProvider implements PreviewObjectProviderInterface
         // TODO: use ContentDataMapper service for setting data to the object
 
         if ($object instanceof SeoInterface) {
-            $this->setSeoData($object, $data);
+            $this->seoDataMapper->map($data, $object, $object);
         }
 
         if ($object instanceof ExcerptInterface) {
-            $this->setExcerptData($object, $data);
+            $this->excerptDataMapper->map($data, $object, $object);
         }
 
         if ($object instanceof TemplateInterface) {
-            $this->setTemplateData($object, $data);
+            $this->templateDataMapper->map($data, $object, $object);
         }
     }
 
     /**
-     * @param mixed[] $data
-     */
-    private function setSeoData(SeoInterface $object, array &$data): void
-    {
-        $seoTitle = 'seoTitle';
-        if (\array_key_exists($seoTitle, $data)) {
-            $value = $data[$seoTitle];
-
-            Assert::nullOrString($value);
-
-            $object->setSeoTitle($value);
-
-            unset($data[$seoTitle]);
-        }
-
-        $seoDescription = 'seoDescription';
-        if (\array_key_exists($seoDescription, $data)) {
-            $value = $data[$seoDescription];
-
-            Assert::nullOrString($value);
-
-            $object->setSeoDescription($value);
-
-            unset($data[$seoDescription]);
-        }
-
-        $seoKeywords = 'seoKeywords';
-        if (\array_key_exists($seoKeywords, $data)) {
-            $value = $data[$seoKeywords];
-
-            Assert::nullOrString($value);
-
-            $object->setSeoKeywords($value);
-
-            unset($data[$seoKeywords]);
-        }
-
-        $seoCanonicalUrl = 'seoCanonicalUrl';
-        if (\array_key_exists($seoCanonicalUrl, $data)) {
-            $value = $data[$seoCanonicalUrl];
-
-            Assert::nullOrString($value);
-
-            $object->setSeoCanonicalUrl($value);
-
-            unset($data[$seoCanonicalUrl]);
-        }
-
-        $seoNoIndex = 'seoNoIndex';
-        if (\array_key_exists($seoNoIndex, $data)) {
-            $value = $data[$seoNoIndex];
-
-            Assert::boolean($value);
-
-            $object->setSeoNoIndex($value);
-
-            unset($data[$seoNoIndex]);
-        }
-
-        $seoNoFollow = 'seoNoFollow';
-        if (\array_key_exists($seoNoFollow, $data)) {
-            $value = $data[$seoNoFollow];
-
-            Assert::boolean($value);
-
-            $object->setSeoNoFollow($value);
-
-            unset($data[$seoNoFollow]);
-        }
-
-        $seoHideInSitemap = 'seoHideInSitemap';
-        if (\array_key_exists($seoHideInSitemap, $data)) {
-            $value = $data[$seoHideInSitemap];
-
-            Assert::boolean($value);
-
-            $object->setSeoHideInSitemap($value);
-
-            unset($data[$seoHideInSitemap]);
-        }
-    }
-
-    /**
-     * @param mixed[] $data
-     */
-    private function setExcerptData(ExcerptInterface $object, array &$data): void
-    {
-        $excerptTitle = 'excerptTitle';
-        if (\array_key_exists($excerptTitle, $data)) {
-            $value = $data[$excerptTitle];
-
-            Assert::nullOrString($value);
-
-            $object->setExcerptTitle($value);
-
-            unset($data[$excerptTitle]);
-        }
-
-        $excerptDescription = 'excerptDescription';
-        if (\array_key_exists($excerptDescription, $data)) {
-            $value = $data[$excerptDescription];
-
-            Assert::nullOrString($value);
-
-            $object->setExcerptDescription($value);
-
-            unset($data[$excerptDescription]);
-        }
-
-        $excerptMore = 'excerptMore';
-        if (\array_key_exists($excerptMore, $data)) {
-            $value = $data[$excerptMore];
-
-            Assert::nullOrString($value);
-
-            $object->setExcerptMore($value);
-
-            unset($data[$excerptMore]);
-        }
-
-        $excerptCategories = 'excerptCategories';
-        if (\array_key_exists($excerptCategories, $data)) {
-            $value = $data[$excerptCategories];
-
-            Assert::isArray($value);
-
-            $categories = $this->categoryFactory->create($value);
-
-            $object->setExcerptCategories($categories);
-
-            unset($data[$excerptCategories]);
-        }
-
-        $excerptTags = 'excerptTags';
-        if (\array_key_exists($excerptTags, $data)) {
-            $value = $data[$excerptTags];
-
-            Assert::isArray($value);
-
-            $tags = $this->tagFactory->create($value);
-
-            $object->setExcerptTags($tags);
-
-            unset($data[$excerptTags]);
-        }
-
-        $excerptImage = 'excerptImage';
-        if (\array_key_exists($excerptImage, $data)) {
-            $value = $data[$excerptImage];
-
-            Assert::nullOrIsArray($value);
-
-            $object->setExcerptImage($value);
-
-            unset($data[$excerptImage]);
-        }
-
-        $excerptIcon = 'excerptIcon';
-        if (\array_key_exists($excerptIcon, $data)) {
-            $value = $data[$excerptIcon];
-
-            Assert::nullOrIsArray($value);
-
-            $object->setExcerptIcon($value);
-
-            unset($data[$excerptIcon]);
-        }
-    }
-
-    /**
-     * @param mixed[] $data
-     */
-    private function setTemplateData(TemplateInterface $object, array &$data): void
-    {
-        $object->setTemplateData($data);
-    }
-
-    /**
+     * {@inheritdoc}
+     *
      * @param ContentProjectionInterface $object
      * @param string $locale
      * @param mixed[] $context
