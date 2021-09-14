c220ec355802bf2256092de5842aeb333449aee9
Alexander Schranz <alexander@sulu.io>
2021-09-14 15:55:38 +0200

    Update .php-cs-fixer.dist.php

--- .php-cs-fixer.dist.php
+++ .php-cs-fixer.dist.php
@@ -9,10 +9,6 @@ This source file is subject to the MIT license that is bundled
 with this source code in the file LICENSE.
 EOF;
 
-$finder = PhpCsFixer\Finder::create()
-    ->exclude(['var/cache', 'tests/Resources/cache', 'node_modules'])
-    ->in(__DIR__);
-
 $config = new PhpCsFixer\Config();
 $config->setRiskyAllowed(true)
     ->setRules([
