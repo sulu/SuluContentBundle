05d92c27391e096005ec6dc844cd97afc84ac131
Alexander Schranz <alexander@sulu.io>
2022-01-25 12:05:19 +0100

    Undo accidently ghostlocale changes

--- Content/Infrastructure/Sulu/Route/ContentRouteDefaultsProvider.php
+++ Content/Infrastructure/Sulu/Route/ContentRouteDefaultsProvider.php
@@ -15,7 +15,6 @@ namespace Sulu\Bundle\ContentBundle\Content\Infrastructure\Sulu\Route;
 
 use Doctrine\ORM\EntityManagerInterface;
 use Doctrine\ORM\NoResultException;
-use Doctrine\ORM\Query\Expr\Join;
 use Sulu\Bundle\ContentBundle\Content\Application\ContentResolver\ContentResolverInterface;
 use Sulu\Bundle\ContentBundle\Content\Domain\Exception\ContentNotFoundException;
 use Sulu\Bundle\ContentBundle\Content\Domain\Model\ContentRichEntityInterface;
@@ -122,15 +121,8 @@ class ContentRouteDefaultsProvider implements RouteDefaultsProviderInterface
             $contentRichEntity = $this->entityManager->createQueryBuilder()
                 ->select('entity')
                 ->from($entityClass, 'entity')
-                ->innerJoin(
-                    'entity.dimensionContents',
-                    'dimensionContent',
-                    Join::WITH,
-                    'dimensionContent.locale = :locale'
-                )
                 ->where('entity = :id')
                 ->setParameter('id', $id)
-                ->setParameter('locale', $locale)
                 ->getQuery()
                 ->getSingleResult();
         } catch (NoResultException $exception) {
