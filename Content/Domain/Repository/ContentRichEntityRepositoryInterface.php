b4cc47bb0fae5a9eff26077b2281f3888b18f172
Alexander Schranz <alexander@sulu.io>
2024-08-09 13:14:07 +0200

    Add interface for ContentRichEntityRepository

---
+++ Content/Domain/Repository/ContentRichEntityRepositoryInterface.php
@@ -0,0 +1,72 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of Sulu.
+ *
+ * (c) Sulu GmbH
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.
+ */
+
+namespace Sulu\Bundle\ContentBundle\Content\Domain\Repository;
+
+use Sulu\Bundle\ContentBundle\Content\Domain\Model\ContentRichEntityInterface;
+
+/**
+ * @template T of ContentRichEntityInterface
+ *
+ * @phpstan-type ContentRichEntityRepositoryFilters array{
+ *     identifiers?: array<string>,
+ *     locale?: string|null,
+ *     stage?: string|null,
+ *     categoryIds?: int[],
+ *     categoryKeys?: string[],
+ *     categoryOperator?: 'AND'|'OR',
+ *     tagIds?: int[],
+ *     tagNames?: string[],
+ *     tagOperator?: 'AND'|'OR',
+ *     templateKeys?: string[],
+ *     loadGhost?: bool,
+ * }
+ * @phpstan-type ContentRichEntityRepositorySortBys array{
+ *     title?: 'asc'|'desc',
+ *     authored?: 'asc'|'desc',
+ *     workflowPublished?: 'asc'|'desc',
+ * }
+ * @phpstan-type ContentRichEntityRepositorySelects array{
+ *     content_admin?: bool,
+ *     content_website?: bool,
+ *     with-excerpt-tags?: bool,
+ *     with-excerpt-categories?: bool,
+ *     with-excerpt-categories-translation?: bool,
+ *     with-excerpt-image?: bool,
+ *     with-excerpt-image-translation?: bool,
+ *     with-excerpt-icon?: bool,
+ *     with-excerpt-icon-translation?: bool,
+ * }
+ */
+interface ContentRichEntityRepositoryInterface
+{
+    /**
+     * @param ContentRichEntityRepositoryFilters $filters
+     * @param ContentRichEntityRepositorySortBys $sortBys
+     * @param ContentRichEntityRepositorySelects $selects
+     *
+     * @return iterable<T>
+     */
+    public function findBy(
+        array $filters = [],
+        array $sortBys = [],
+        array $selects = [],
+    ): iterable;
+
+    /**
+     * @param ContentRichEntityRepositoryFilters $filters
+     */
+    public function countBy(
+        array $filters = [],
+    ): int;
+}
